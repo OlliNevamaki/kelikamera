36f9029c9bb39848cc92bbf181e238267980fef0
anttonipekkarinen <anttoni.pekkarinen@tuni.fi>
2024-05-11 12:00:17 +0300

    Css tyylittelyä tallennussivulle

--- frontend/src/SavedRoutes.jsx
+++ frontend/src/SavedRoutes.jsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import './App.css';
+
 /**Function for a page view that displays the saved routes from Maps view and also has options to delete them or review them */
 const SavedRoutes = () => {
   const [savedRoutes, setSavedRoutes] = useState([]);
   const navigate = useNavigate();
+
   /**Getting the saved routes from the localStorage */
   useEffect(() => {
     const savedRoutesFromStorage =
       JSON.parse(localStorage.getItem("savedRoutes")) || [];
     setSavedRoutes(savedRoutesFromStorage);
   }, []);
+
   /**Function for deleting a saved route according to the saved items index and updating the list after */
   const deleteRoute = (index) => {
     const updatedRoutes = [...savedRoutes];
@@ -18,12 +22,14 @@ const SavedRoutes = () => {
     setSavedRoutes(updatedRoutes);
     localStorage.setItem("savedRoutes", JSON.stringify(updatedRoutes));
   };
+
   /**Function for the button to store the origin and destination values in state object so they can be referred in Maps component when it's opened */
   const openSavedRoute = (route) => {
     navigate("/suunnittele-reitti", {
       state: { origin: route.origin, destination: route.destination },
     });
   };
+
   /**Function to open the Maps view */
   const openMapView = () => {
     navigate("/suunnittele-reitti");
@@ -32,18 +38,18 @@ const SavedRoutes = () => {
   return (
     <>
       <div>
+        <button className="plan-button" onClick={openMapView}>Suunnittele reitti</button>
         <h2>Tallennetut reitit</h2>
-        <ul>
+        <ul className="location-list">
           {savedRoutes.map((route, index) => (
-            <li key={index}>
-              <p>Lähtöpaikka: {route.origin}</p>
-              <p>Määränpää: {route.destination}</p>
-              <button onClick={() => deleteRoute(index)}>Poista</button>
-              <button onClick={() => openSavedRoute(route)}>Avaa reitti</button>
+            <li key={index} className="location-item">
+              <p className="from-button">Lähtöpaikka: {route.origin}</p>
+              <p className="to-button">Määränpää: {route.destination}</p>
+              <button className="delete-button" onClick={() => deleteRoute(index)}>Poista</button>
+              <button className="open-button" onClick={() => openSavedRoute(route)}>Avaa reitti</button>
             </li>
           ))}
         </ul>
-        <button onClick={openMapView}>Suunnittele reitti</button>
         <Footer />
       </div>
     </>
@@ -51,3 +57,5 @@ const SavedRoutes = () => {
 };
 
 export default SavedRoutes;
+
+
